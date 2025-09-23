84b7e6f9c5eb805a8427e79a3636f913d0a151f7
nishidhparmar <nishidhparmar1310@gmail.com>
2025-09-23 16:39:39 +0530

    faet: hero and que section chagnes for img

--- src/sections/HeroSection.tsx
+++ src/sections/HeroSection.tsx
@@ -7,27 +7,30 @@ import React from 'react'
 
 const HeroSection = () => {
     return (
-        <Container>
-            <div className={cn(`h-screen bg-[url(/images/hero.jpg)] bg-cover bg-center`)}>
-                <div className='w-1/2 flex items-start justify-center h-full flex-col gap-4'>
-                    <WhiteGradientText className='text-[24px] font-semibold font-primary'>
-                        (Ask with faith)
-                    </WhiteGradientText>
-                    <GYGradientText className='text-[56px] font-semibold font-primary'>
-                        Hear Gita guided answers like Krishna would counsel.
-                    </GYGradientText>
-                    <div className='w-full h-[1px] bg-gradient-to-r from-[#FFFFFF] to-[#99999900]' ></div>
-                    <WhiteGradientText>
-                        Krishna Vaani is your gentle daily companion for your family’s questions scripture aligned, child friendly, and multilingual.
-                    </WhiteGradientText>
-                    <Button>Experience Krishna</Button>
+        <div className='bg-[url(/images/hero.jpg)] bg-fixed  w-full h-full bg-no-repeat bg-cover'>
+            <Container>
+                <div className={cn(`h-screen `)}>
+                    <div className='w-1/2 flex items-start justify-center h-full flex-col gap-4'>
+                        <WhiteGradientText className='text-[24px] font-semibold font-primary'>
+                            (Ask with faith)
+                        </WhiteGradientText>
+                        <GYGradientText className='text-[56px] font-semibold font-primary'>
+                            Hear Gita guided answers like Krishna would counsel.
+                        </GYGradientText>
+                        <div className='w-full h-[1px] bg-gradient-to-r from-[#FFFFFF] to-[#99999900]' ></div>
+                        <WhiteGradientText>
+                            Krishna Vaani is your gentle daily companion for your family’s questions scripture aligned, child friendly, and multilingual.
+                        </WhiteGradientText>
+                        <Button>Experience Krishna</Button>
 
-                </div>
-                <div className='w-1/2'>
+                    </div>
+                    <div className='w-1/2'>
 
+                    </div>
                 </div>
-            </div>
-        </Container>
+            </Container>
+        </div>
+
     )
 }
 
--- src/sections/QuestionSection.tsx
+++ src/sections/QuestionSection.tsx
@@ -8,34 +8,36 @@ import React from 'react'
 
 const QuestionSection = () => {
     return (
-        <Container>
-            <div className={cn(`h-screen bg-[url(/images/hero.jpg)] bg-cover bg-center`)}>
-                <div className='w-1/2 flex items-start justify-center h-full flex-col gap-4'>
-                    <WhiteGradientText className='text-[24px] font-semibold font-primary'>
-                        (Ask with faith)
-                    </WhiteGradientText>
-                    <GYGradientText className='text-[56px] font-semibold font-primary'>
-                        Ask from the heart. Receive counsel rooted in the Gita.
-                    </GYGradientText>
-                    <div className='w-full h-[1px] bg-gradient-to-r from-[#FFFFFF] to-[#99999900]' ></div>
-                    <div className='flex items-center justify-center gap-3'>
-                        <PankhVector />
-                        <QuestionCard text='Krishna, how can I find true happiness?' />
+        <div className='bg-[url(/images/hero.jpg)] bg-fixed  w-full h-full bg-no-repeat bg-cover'>
+            <Container>
+                <div className={cn(`h-screen  bg-cover bg-center`)}>
+                    <div className='w-1/2 flex items-start justify-center h-full flex-col gap-4'>
+                        <WhiteGradientText className='text-[24px] font-semibold font-primary'>
+                            (Ask with faith)
+                        </WhiteGradientText>
+                        <GYGradientText className='text-[56px] font-semibold font-primary'>
+                            Ask from the heart. Receive counsel rooted in the Gita.
+                        </GYGradientText>
+                        <div className='w-full h-[1px] bg-gradient-to-r from-[#FFFFFF] to-[#99999900]' ></div>
+                        <div className='flex items-center justify-center gap-3'>
+                            <PankhVector />
+                            <QuestionCard text='Krishna, how can I find true happiness?' />
+                        </div>
+                        <div className='flex items-center justify-center gap-3'>
+                            <PankhVector />
+                            <QuestionCard text='Krishna, how should I deal with expectations from my work?' />
+                        </div>
+                        <div className='flex items-center justify-center gap-3'>
+                            <PankhVector />
+                            <QuestionCard text='Krishna, why do I feel hurt when others don’t respect me?' />
+                        </div>
                     </div>
-                    <div className='flex items-center justify-center gap-3'>
-                        <PankhVector />
-                        <QuestionCard text='Krishna, how should I deal with expectations from my work?' />
-                    </div>
-                    <div className='flex items-center justify-center gap-3'>
-                        <PankhVector />
-                        <QuestionCard text='Krishna, why do I feel hurt when others don’t respect me?' />
-                    </div>
-                </div>
-                <div className='w-1/2'>
+                    <div className='w-1/2'>
 
+                    </div>
                 </div>
-            </div>
-        </Container>
+            </Container>
+        </div>
     )
 }
 
