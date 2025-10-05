7cebba2736f458b2f51e4da7b889f4dae946110c
nishidhparmar <nishidhparmar1310@gmail.com>
2025-10-05 14:59:55 +0530

    feat: video added

--- src/sections/QuestionSection.tsx
+++ src/sections/QuestionSection.tsx
@@ -8,50 +8,82 @@ import WhiteGradientText from '@/components/texts/WhiteGradientText'
 import { cn } from '@/utils/cn'
 import { useGSAP } from '@gsap/react'
 import gsap from 'gsap'
+import Image from 'next/image'
+import ReactPlayer from 'react-player'
 
-import React, { useRef } from 'react'
+import React, { useRef, useState } from 'react'
 
 const QuestionSection = () => {
 
     const containerRef = useRef(null)
+    const [isPlay, setIsPlay] = useState(false)
     useGSAP(() => {
         const t1 = gsap.timeline()
-        t1.fromTo('.text', {
-            xPercent: -20,
-            opacity: 0,
+        t1.
+            fromTo('.mandala', {
+                opacity: 0,
+                scale: 0
 
-        }, {
-            xPercent: 0,
-            opacity: 1,
-            scrollTrigger: {
-                trigger: '.text',
-                start: 'top 90%',
-                end: '+=100',
-                scrub: 2
-            }
+            }, {
+                scale: 1,
+                opacity: 1,
+                duration: 2,
+                scrollTrigger: {
+                    trigger: '.mandala',
+                    start: 'top 60%',
+                    end: '+=0',
+                    scrub: 2,
+                    once: true
+                }
 
-        }).fromTo('.que', {
-            xPercent: -20,
-            opacity: 0,
-            duration: 1,
+            }).
+            to('.mandala', {
+                rotate: 360,
+                repeat: -1,
+                duration: 120,
+                transformOrigin: 'center'
 
-        }, {
-            xPercent: 0,
-            opacity: 1,
-            duration: 1,
-            stagger: 0.4,
-            scrollTrigger: {
-                trigger: '.que',
-                start: 'top 90%',
-                end: 'center 60%',
-                scrub: 2
-            }
+            }).
+            fromTo('.text', {
+                xPercent: -20,
+                opacity: 0,
 
-        })
+            }, {
+                xPercent: 0,
+                opacity: 1,
+                scrollTrigger: {
+                    trigger: '.text',
+                    start: 'top 90%',
+                    end: '+=100',
+                    scrub: 2
+                }
+
+            }).fromTo('.que', {
+                xPercent: -20,
+                opacity: 0,
+                duration: 1,
+
+            }, {
+                xPercent: 0,
+                opacity: 1,
+                duration: 1,
+                stagger: 0.4,
+                scrollTrigger: {
+                    trigger: '.que',
+                    start: 'top 90%',
+                    end: 'center 60%',
+                    scrub: 2
+                }
+
+            })
 
     }, { scope: containerRef })
     return (
-        <div ref={containerRef} className='bg-[url(/images/hero.jpg)] bg-fixed  w-full h-full bg-no-repeat bg-cover'>
+        <div ref={containerRef} className='overflow-hidden bg-[url(/images/hero-blank.png)] bg-fixed  w-full h-full bg-no-repeat bg-cover relative'>
+            <Image alt='mandala' src={'/images/circular-mandala.svg'} width={1000} height={1000} className='w-full h-full absolute top-0 translate-x-[25%] mandala' />
+            <ReactPlayer playing={isPlay} onEnded={() => {
+                setIsPlay(false)
+            }} src='/v1.webm' width={'100%'} height={'100%'} className='w-full h-full absolute translate-x-[22%] bottom-0 translate-y-[10%]' />
             <Container>
                 <div className={cn(`h-screen  bg-cover bg-center`)}>
                     <div className='w-1/2 flex items-start justify-center h-full flex-col gap-4'>
@@ -65,7 +97,11 @@ const QuestionSection = () => {
                         </div>
 
                         <div className='w-full h-[1px] bg-gradient-to-r from-[#FFFFFF] to-[#99999900]' ></div>
-                        <div className='flex items-center justify-center gap-3 que -translate-x-[100]'>
+                        <div
+                            onClick={() => {
+                                setIsPlay(true)
+                            }}
+                            className='flex items-center justify-center gap-3 que -translate-x-[100]'>
                             <PankhVector />
                             <QuestionCard text='Krishna, how can I find true happiness?' />
                         </div>
