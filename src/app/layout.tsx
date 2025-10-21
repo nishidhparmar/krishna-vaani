db39134a0c0f1f00a4be991a2a307b49ed8325ab
nishidhparmar <nishidhparmar1310@gmail.com>
2025-10-21 15:20:06 +0530

    feat: gtag

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import "./globals.css";
 import { ToastContainer } from "react-toastify";
+import Script from "next/script";
 
 
 export const metadata: Metadata = {
@@ -15,9 +16,30 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        {/* ✅ Google Tag Manager Script (HEAD section) */}
+        <Script id="gtm-head" strategy="afterInteractive">
+          {`
+            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-MF6BK53D');
+          `}
+        </Script>
+      </head>
       <body
         className={`antialiased`}
       >
+        {/* ✅ Google Tag Manager (noscript) - BODY section */}
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-MF6BK53D"
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          ></iframe>
+        </noscript>
         <ToastContainer
           position="top-center"
           theme="dark" />
